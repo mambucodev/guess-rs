4103354c7e098e79167316b6692b6e4665579e3d
Mambuco <gabriele.giambrone@icloud.com>
2025-05-15 11:42:23 +0200

    Few bug fixed and features

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@ fn main() {
     let mut lives: usize = 5;
     let mut level: usize = 1;
 
+    let heart = "󰋑";
+
     // Display game instructions
     println!("{}", "Welcome to the Number Guessing Game!".green().bold());
     println!("Instructions:");
@@ -26,6 +28,8 @@ fn main() {
     println!();
 
     loop {
+        let mut inbox: Vec<String> = Vec::new();
+
         println!(
             "{}",
             format!(
@@ -36,9 +40,10 @@ fn main() {
             )
             .green()
         );
+
         println!(
             "{}",
-            format!("Lives remaining: {}", "❤".repeat(lives)).red()
+            format!("Lives remaining: {}", heart.repeat(lives)).red()
         );
 
         let index = rand::thread_rng().gen_range(1..=maximum);
@@ -49,92 +54,106 @@ fn main() {
             print!("{} ", ">".cyan().bold());
             io::stdout().flush().expect("Failed to flush stdout.");
 
-            io::stdin()
-                .read_line(&mut guess)
-                .expect("Failed to read line.");
+            match io::stdin().read_line(&mut guess) {
+                Ok(_) => {
+                    if guess.trim().to_lowercase() == "exit" {
+                        println!("{}", "Game over. Thanks for playing!".red().bold());
+                        return;
+                    }
 
-            if guess.trim().to_lowercase() == "exit" {
-                println!("{}", "Game over. Thanks for playing!".red().bold());
-                return;
-            }
+                    let parsed_guess = guess.trim().parse::<u32>();
+                    match parsed_guess {
+                        Ok(n) => {
+                            if (n as usize) > maximum {
+                                inbox.push(format!(
+                                    "{}",
+                                    format!("Please guess a number between 1 and {maximum}.")
+                                        .red()
+                                        .bold()
+                                ));
+                                break;
+                            }
+                            match (n as usize).cmp(&index) {
+                                Ordering::Less => {
+                                    inbox.push(format!(
+                                        "{}",
+                                        "Too low! Try a higher number.".red().bold()
+                                    ));
+                                    lives -= 1;
+                                }
+                                Ordering::Greater => {
+                                    inbox.push(format!(
+                                        "{}",
+                                        "Too high! Try a lower number.".red().bold()
+                                    ));
+                                    lives -= 1;
+                                }
+                                Ordering::Equal => {
+                                    println!(
+                                        "{} It was {}.",
+                                        "Correct!".green().bold(),
+                                        n.to_string().yellow().bold()
+                                    );
+                                    level += 1;
+                                    maximum += 10;
+                                    lives += (level as f32 / 2.0).round() as usize;
+                                    inbox.push(format!(
+                                        "{}",
+                                        format!(
+                                            "You've advanced to level {} and gained an extra life!",
+                                            level
+                                        )
+                                        .yellow()
+                                        .italic()
+                                    ));
+                                    break 'inner;
+                                }
+                            }
+                        }
+                        Err(e) => match e.kind() {
+                            std::num::IntErrorKind::PosOverflow => {
+                                inbox.push(format!(
+                                    "{}",
+                                    format!(
+                                        "Number too large. Please enter a number below {}.",
+                                        u32::MAX
+                                    )
+                                    .red()
+                                ));
+                            }
+                            _ => {
+                                inbox.push(format!(
+                                    "{}",
+                                    "Invalid input. Please enter a valid number.".red().bold()
+                                ));
+                            }
+                        },
+                    };
 
-            let parsed_guess = guess.trim().parse::<u32>();
-            match parsed_guess {
-                Ok(n) => {
-                    if (n as usize) > maximum {
+                    if lives == 0 {
                         println!(
                             "{}",
-                            format!("Please guess a number between 1 and {maximum}.").red()
+                            format!("Game Over, it was {index}! You made it to level {}.", level)
+                                .red()
+                                .bold()
                         );
-                        continue;
+                        return;
                     }
-                    match (n as usize).cmp(&index) {
-                        Ordering::Less => {
-                            println!("{}", "Too low! Try a higher number.".red().bold());
-                            lives -= 1;
-                        }
-                        Ordering::Greater => {
-                            println!("{}", "Too high! Try a lower number.".red().bold());
-                            lives -= 1;
-                        }
-                        Ordering::Equal => {
-                            println!(
-                                "{} It was {}.",
-                                "Correct!".green().bold(),
-                                n.to_string().yellow().bold()
-                            );
-                            level += 1;
-                            maximum += 10;
-                            lives += (level as f32 / 2.0).round() as usize;
-                            println!(
-                                "{}",
-                                format!(
-                                    "You've advanced to level {} and gained an extra life!",
-                                    level
-                                )
-                                .yellow()
-                                .italic()
-                            );
-                            break 'inner;
-                        }
-                    }
+
+                    break;
                 }
-                Err(e) => match e.kind() {
-                    std::num::IntErrorKind::PosOverflow => {
-                        println!(
-                            "{}",
-                            format!(
-                                "Number too large. Please enter a number below {}.",
-                                u32::MAX
-                            )
-                            .red()
-                        );
-                    }
-                    _ => {
-                        println!(
-                            "{}",
-                            "Invalid input. Please enter a valid number.".red().bold()
-                        );
-                    }
-                },
-            };
 
-            if lives == 0 {
-                println!(
-                    "{}",
-                    format!("Game Over, it was {index}! You made it to level {}.", level)
-                        .red()
-                        .bold()
-                );
-                return;
+                Err(e) => {
+                    println!("{}\n{}", "Error reading line:".red().bold(), e);
+                    return;
+                }
             }
-
-            println!(
-                "{}",
-                format!("Lives remaining: {}", "❤".repeat(lives)).red()
-            );
         }
 
         clear();
+
+        for message in &inbox {
+            println!("{}", message);
+        }
     }
 }
